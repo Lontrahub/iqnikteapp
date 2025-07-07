3296eb7c3aeb4e63c229cc866dd5f0685ea9fe42
Otter Life <otterlife55@gmail.com>
2025-07-07 07:17:01 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,7 +1,8 @@
+
 "use client"
 
 import * as React from "react"
-import { ChevronLeft, ChevronRight } from "phosphor-react"
+import { CaretLeft, CaretRight } from "phosphor-react"
 import { DayPicker } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
@@ -55,10 +56,10 @@ function Calendar({
       }}
       components={{
         IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
+          <CaretLeft className={cn("h-4 w-4", className)} {...props} />
         ),
         IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
+          <CaretRight className={cn("h-4 w-4", className)} {...props} />
         ),
       }}
       {...props}
